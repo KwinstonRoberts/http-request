c83eee2dac83f013dace95bbb2ea96e9136b3f53
kyle roberts <kyle.w.r@hotmail.com>
2017-07-31 13:18:40 +0000

    made a full module

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -1,1 +1,12 @@
-print_lowercase.js
+var getHTML = require('../http-request/http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printLowerCase (html) {
+  console.log(html.toLowerCase());
+}
+
+getHTML(requestOptions,printLowerCase);
